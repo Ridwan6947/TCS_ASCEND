a62256dc119bca338c87c8e42ae8b628ed1bf7a6
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2025-02-07 11:16:59 +0530

    Update IPA12.java
    IPA17 answer , do check

--- IPA12.java
+++ IPA12.java
@@ -1,76 +1,104 @@
 import java.util.*;
 
-public class IPA12 {
+class Student{
+    private int rollNo;
+    private String name;
+    private String subject;
+    private char grade;
+    private String date;
+
+    public Student(int rollNo , String name , String subject , char grade , String date){
+        this.rollNo = rollNo;
+        this.name = name;
+        this.subject = subject;
+        this.grade = grade;
+        this.date = date;
+    }
+    public int getRollNo(){
+        return rollNo;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getSubject(){
+        return subject;
+    }
+    public char getGrade(){
+        return grade;
+    }
+    public String getDate(){
+        return date;
+    }
+    public void setRollNo(int rollNo){
+        this.rollNo = rollNo;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+    public void setGrade(char grade){
+        this.grade = grade;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+}
+
+public class Main{
+
+    public static Student[] findStudentByGradeAndMonth(Student[] arr , char gr , int  mo){
+        ArrayList<Student> list = new ArrayList<>();
+
+        for(int i = 0 ; i < arr.length ; i++){
+            String[] month =arr[i].getDate().split("/");
+            int studentMonth = Integer.parseInt(month[1]);
+            if(arr[i].getGrade() == gr && studentMonth == mo){
+                list.add(arr[i]);
+            }
+        }
+        Student [] ans1 = new Student[list.size()];
+        Iterator<Student> it = list.iterator();
+        int index = 0;
+        while(it.hasNext()){
+        	ans1[index++] = it.next();
+        }
+       
+        for(int i = 0 ; i < ans1.length-1 ; i++){
+            for(int j = 0 ; j < ans1.length - i - 1 ; j++){
+                if(ans1[j].getRollNo() > ans1[j+1].getRollNo()){
+                    Student temp = ans1[j];
+                    ans1[j] = ans1[j+1];
+                    ans1[j+1] = temp;
+                }
+            }
+        }
+        return ans1;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Medicine[] medicines = new Medicine[4];
-        for(int i = 0 ; i < medicines.length ; i++){
-            String a = sc.nextLine();
+        Student [] student = new Student[4];
+        for(int i = 0 ; i < student.length ; i++){
+            int a = sc.nextInt();
+            sc.nextLine();
             String b = sc.nextLine();
             String c = sc.nextLine();
-            int d = sc.nextInt();
-            sc.nextLine();
-            medicines[i] = new Medicine(a,b,c,d);
+            char d = sc.nextLine().charAt(0);
+            String e = sc.nextLine();
+            student[i] = new Student(a,b,c,d,e);
         }
-        String diseaseToFind = sc.nextLine();
-        int[] ans = getPriceByDisease(medicines , diseaseToFind);
+        char gradeToFind = sc.nextLine().charAt(0);
+        int monthToFind = sc.nextInt();sc.nextLine();
+        Student[] ans = findStudentByGradeAndMonth(student , gradeToFind , monthToFind);
         if(ans != null){
-            for(int i = 0 ; i < ans.length ; i++){
-                System.out.println(ans[i]);
+            for(Student students : ans){
+                System.out.println(students.getName());
             }
+            System.out.println(ans.length);
+        }else{
+            System.out.println("null");
         }
-    }
-    public static int[] getPriceByDisease(Medicine[] arr , String target){
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i].getDisease().equals(target)){
-                ans.add(arr[i].getPrice());
-            }
-        }
-        int [] ansArray = new int[ans.size()];
-        for(int i = 0 ; i < ans.size() ; i++){
-            ansArray[i] = ans.get(i);
-        }
-        
-        return ansArray;
-    }    
-}
-
-class Medicine{
-    private String medicineName;
-    private String batch;
-    private String disease;
-    private int price;
-
-    public Medicine(String medicineName , String batch , String disease , int price){
-        this.medicineName = medicineName;
-        this.batch = batch;
-        this.disease = disease;
-        this.price = price;
-    }
-
-    public String getMedicineName(){
-        return medicineName;
-    }
-    public String getBatch(){
-        return batch;
-    }
-    public String getDisease(){
-        return disease;
-    }
-    public int getPrice(){
-        return price;
-    }
-    public void setMedicineName(String medicineName){
-        this.medicineName = medicineName;
-    }
-    public void setBatch(String batch){
-        this.batch = batch;
-    }
-    public void setDisease (String disease){
-        this.disease = disease;
-    }
-    public void setPrice( int price){
-        this.price = price;
     }
 }
+ 
